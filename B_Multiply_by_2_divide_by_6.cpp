2b513530359a2351b45e723c83559abf8a284e91
Anish Mehta <mehtaanish111@gmail.com>
2024-10-12 22:55:45 +0530

    1347B

---
+++ B_Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#define ll long long int
+
+using namespace std;
+
+int main() {
+    int t; 
+    cin >> t;
+    
+    while (t--) {
+    ll n;
+    cin >> n;
+    int c=0;
+    while(n>=1){
+        if(n==1) break;
+        if(n%6==0){
+            n/=6;
+            c++;
+        }
+        else{
+            n*=2;
+            c++;
+        }
+    }
+
+    if(n==1) cout << c << endl;
+    else cout << -1 << endl;
+     
+    }
+
+    return 0;
+}
